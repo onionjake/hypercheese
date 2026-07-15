748c07a2ba618947f1b1dc39f129a8a7096d1412
Jewel <github@thesteve.org>
2026-07-14 21:46:34 -0600

    Load S3 config from Creds

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -9,17 +9,40 @@ class Bucket
     def setup_bucket
       require 'aws-sdk-s3'
 
+      s3_config = Rails.application.credentials[:s3] || {}
+
+      access_key_id = s3_config[:access_key_id]
+      secret_access_key = s3_config[:secret_access_key]
+      bucket_name = s3_config[:bucket]
+      endpoint = s3_config[:endpoint]
+      region = s3_config[:region] || 'us-east-1'
+      force_path_style = s3_config.key?(:force_path_style) ? s3_config[:force_path_style] : true
+
+      if Rails.env.development?
+        access_key_id ||= 'minioadmin'
+        secret_access_key ||= 'minioadmin'
+        bucket_name ||= 'hypercheese'
+        endpoint ||= 'http://localhost:9000'
+      end
+
+      if Rails.env.production?
+        missing = []
+        missing << 's3.access_key_id' if access_key_id.blank?
+        missing << 's3.secret_access_key' if secret_access_key.blank?
+        missing << 's3.bucket' if bucket_name.blank?
+
+        if missing.any?
+          raise KeyError, "Missing Rails credentials: #{missing.join(', ')}"
+        end
+      end
+
       Aws.config.update({
-        region: Rails.env.production? ? 'us-east-1' : 'us-east-1',
-        credentials: Aws::Credentials.new(
-          Rails.env.production? ? ENV['AWS_ACCESS_KEY_ID'] : 'minioadmin',
-          Rails.env.production? ? ENV['AWS_SECRET_ACCESS_KEY'] : 'minioadmin'
-        ),
-        endpoint: Rails.env.production? ? nil : 'http://localhost:9000',
-        force_path_style: true,
+        region: region,
+        credentials: Aws::Credentials.new(access_key_id, secret_access_key),
+        endpoint: endpoint,
+        force_path_style: force_path_style,
       })
 
-      bucket_name = Rails.env.production? ? ENV['AWS_BUCKET_NAME'] : 'hypercheese'
       bucket = Aws::S3::Resource.new.bucket bucket_name
 
       # Ensure bucket exists in development
